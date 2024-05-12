015a5c146c3bdee5d4317ece081e1e2eda82c5a7
Saikat Sardar <saikatsardar5@gmail.com>
2024-05-13 02:44:59 +0530

    some configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+// import Display from "@/components/Display";
+// import Nav from "@/components/Nav";
+
+import Head from "next/head";
+
 const index = () => {
+  console.log(process.env.NEXT_PUBLIC_APIURL);
+
   return (
     <>
-      <div className="">Hi</div>
+      <Head>
+        <title>RandomUser V2</title>
+      </Head>
+      {/* <Nav /> */}
+
+      <section className="mx-auto max-w-screen-xl">{/* <Display /> */}</section>
     </>
   );
 };
