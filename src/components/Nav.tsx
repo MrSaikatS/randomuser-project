2e72d03b8d671928840ac3631901b11adcf7b70b
Saikat Sardar <saikatsardar5@gmail.com>
2024-05-20 23:09:40 +0530

    done

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,13 +8,20 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from "@nextui-org/navbar";
 import { useQueryClient } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { Download } from "lucide-react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import DarkModeButton from "./DarkModeButton";
 
 const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const [num, setNum] = useAtom(numAtom);
 
   const queryClient = useQueryClient();
@@ -34,14 +41,28 @@ const Nav = () => {
 
     console.log(fData);
 
+    setIsMenuOpen(false);
+
     queryClient.refetchQueries();
   };
 
   return (
     <>
-      <Navbar maxWidth="xl" isBordered isBlurred className="h-[10vh]">
+      <Navbar
+        maxWidth="xl"
+        className="h-[10vh]"
+        isBlurred
+        isBordered
+        isMenuOpen={isMenuOpen}
+        onMenuOpenChange={setIsMenuOpen}
+      >
         <NavbarBrand className="text-2xl font-bold">RandomUser V2</NavbarBrand>
+
         <NavbarContent justify="end">
+          <NavbarMenuToggle />
+        </NavbarContent>
+
+        <NavbarContent justify="end" className="hidden">
           <NavbarItem>
             <form
               onSubmit={handleSubmit(setUserNumberFn)}
@@ -69,7 +90,43 @@ const Nav = () => {
               </Button>
             </form>
           </NavbarItem>
+          <NavbarItem>
+            <DarkModeButton />
+          </NavbarItem>
         </NavbarContent>
+
+        <NavbarMenu className="items-center justify-center gap-4">
+          <NavbarMenuItem>
+            <form
+              onSubmit={handleSubmit(setUserNumberFn)}
+              className="flex gap-2"
+              noValidate
+            >
+              <Input
+                type="number"
+                defaultValue="3"
+                // variant="underlined"
+                label="Enter Number"
+                labelPlacement="outside-left"
+                {...register("userNumber")}
+                errorMessage={errors.userNumber?.message}
+                isInvalid={errors.userNumber?.message ? true : false}
+              />
+              <Button
+                isLoading={isSubmitting}
+                type="submit"
+                color="primary"
+                variant="shadow"
+                endContent={<Download />}
+              >
+                {isSubmitting ? "" : "Get"}
+              </Button>
+            </form>
+          </NavbarMenuItem>
+          <NavbarMenuItem>
+            <DarkModeButton />
+          </NavbarMenuItem>
+        </NavbarMenu>
       </Navbar>
     </>
   );
